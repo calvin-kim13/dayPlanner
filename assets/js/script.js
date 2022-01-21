dce495bc075667f00dbe8201eaafa48d85dc54e4
Calvin Kim <calvinkim1230@gmail.com>
2022-01-21 02:14:16 -0800

    minor updates to html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
 // VARIABLES
-var timeDisplayEl = $('#currentDay');
+let timeDisplayEl = $('#currentDay');
 
 // TIME AND DATE FUNCTION IN HEADER
 function runningTime() {
-    var currentTimeDate = (moment().format('dddd, MMMM D, YYYY [at] h:mm:ss a'));
+    let currentTimeDate = (moment().format('dddd, MMMM D, YYYY [at] h:mm:ss a'));
     timeDisplayEl.text(currentTimeDate);
     setInterval(runningTime, 1000);
 }
 
+// For Loop
+$('#8').children('textarea').val(localStorage.getItem('8'))
+$('#9').children('textarea').val(localStorage.getItem('9'))
 
-
+// add functionality to save buttons
+$('.saveBtn').on('click', function () {
+    const time = $(this).parent().attr('id')
+    const value = $(this).siblings('textarea').val()
+    localStorage.setItem(time, value)
+    console.log(time, value)
+})
 
 
 
